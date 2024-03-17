97ba94ec2323ea63b60e18e6227f992a8dd17f75
chelsevanatter <chelsevanatter@gmail.com>
2024-03-16 20:05:11 -0700

    Added comments toStiffnessPlotter.py file and data_subscriber_gui.py files

--- StiffnessPlotter.py
+++ StiffnessPlotter.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python3
+
+# StiffnessPlotter.py
+#
+# Chelse VanAtter
+#
+# Code to plot displacement vs force and calculate sitffness from csv files
+
+# Import csv library, plotting library, numpy, and linear regression tools
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,9 +15,11 @@ from scipy.stats import linregress
 # Define the file name for the CSV file
 CSV_FILE = 'arduino_data_subscriber.csv'
 
-# Lists to store the data
+# Declare lists to store the load cell force data and motor steps displacmenet data
 all_load_cell_readings = []
 all_motor_steps = []
+
+# Declare lists that will only have force readings greater than 50 gf since data below 50 gf is noisy
 load_cell_readings = []
 motor_steps = []
 
@@ -23,11 +34,11 @@ with open(CSV_FILE, 'r') as file:
             load_cell_readings.append(float(row[2]))
             motor_steps.append(float(row[3]))
 
-# Plot the data
+# Plot the filtered displacement and force data for points that had a force reading greater than 50 gf
 plt.plot(motor_steps, load_cell_readings, marker='o', linestyle='-')
 plt.title('Force vs. Displacement')
 plt.xlabel('Displacement (mm)')
-plt.ylabel('Force (g)')
+plt.ylabel('Force (gf)')
 plt.xlim([0, 50])
 plt.ylim([0, 1200])
 plt.grid(True)
@@ -38,16 +49,17 @@ line = slope * np.array(motor_steps) + intercept
 plt.plot(motor_steps, line, color='red', linestyle='--', label='Regression Line')
 
 # Annotate the plot with slope and R-squared value
-plt.text(20, 1000, f'Slope: {slope:.2f}', fontsize=12, color='blue')
+plt.text(20, 1000, f'Slope/Stiffness: {slope:.2f}', fontsize=12, color='blue')
 plt.text(20, 800, f'R-squared: {r_value**2:.2f}', fontsize=12, color='blue')
 
 plt.legend()
 plt.show()
 
+# Plot the unfiltered data without linear regression
 plt.plot(all_motor_steps, all_load_cell_readings, marker='o', linestyle='-')
 plt.title('All Force vs. Displacement')
 plt.xlabel('Displacement (mm)')
-plt.ylabel('Force (g)')
+plt.ylabel('Force (gf)')
 plt.xlim([0, 50])
 plt.ylim([0, 1200])
 plt.grid(True)
--- src/blueberry_dc/data_subscriber_gui.py
+++ src/blueberry_dc/data_subscriber_gui.py
@@ -1,41 +1,59 @@
+#!/usr/bin/env python3
+
+# StiffnessPlotter.py
+#
+# Chelse VanAtter and Jostan Brown
+#
+# Code to plot displacement vs force and calculate sitffness from csv files
+
+# Import ROS2 stuff
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float64MultiArray
 from std_srvs.srv import Trigger
-import csv
+
+# Import other necessary libraries
 import time
 import subprocess
 import signal
-import os
 import glob
 import threading
 from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QProgressBar, QLabel, QSpinBox, QPlainTextEdit
 import sys
 
-
+# This is the class for recording bag files with all of the topics we need
 class BagRecorder:
+    # Initialize variables for bag recording class
     def __init__(self, topics):
         self.topics = topics
         self.process = None
 
+    # This function gives the command to start recording the bag file
     def start_recording(self, bag_name):
         cmd = ['ros2', 'bag', 'record', '-o', bag_name] + self.topics
         self.process = subprocess.Popen(cmd)
         print(f'Started recording topics: {self.topics} into bag: {bag_name}')
         print("command: ", cmd)
 
+    # This function gives the command to stop recording the bag file
     def stop_recording(self):
         if self.process:
             self.process.send_signal(signal.SIGINT)  # Send SIGINT to mimic Ctrl+C
             self.process.wait()  # Wait for the process to terminate
             print('Stopped recording.')
-            
+
+# This class subscribes to all of the data and creates clients for the services to move the linear actuators            
 class ROSDataSubscriber(Node):
+    # Initialize variables and create clients
     def __init__(self):
         super().__init__('data_subscriber')
 
+        # This is the distance from the ground to the start of the linear actuator mounted on the Husky robot in millimeters
         self.actuator_height = 430
-        self.bag_file_dir = '/home/jostan/bag_files/'
+        
+        # This needs to be changed depending on who is running the code
+        self.bag_file_dir = '/home/chelse/ros2_ws_ROB599_Project/bag_files/'
+        
         self.inactivity_timeout = 3.0
 
         self.load_cell_force = 0
@@ -46,22 +64,24 @@ class ROSDataSubscriber(Node):
         self.move_in_client = self.create_client(Trigger, '/blueberry_dc/move_in')
         self.move_out_client = self.create_client(Trigger, '/blueberry_dc/move_out')
         self.run_trial_client = self.create_client(Trigger, '/blueberry_dc/run_trial')
+        
         while not self.move_up_client.wait_for_service(timeout_sec=1.0):
             # self.get_logger().info('service not available, waiting again...')
-            print('service 1 not available, waiting again...')
+            print('move_up service not available, waiting again...')
         while not self.move_down_client.wait_for_service(timeout_sec=1.0):
             # self.get_logger().info('service not available, waiting again...')
-            print('service 2 not available, waiting again...')
+            print('move_down service not available, waiting again...')
         while not self.run_trial_client.wait_for_service(timeout_sec=1.0):
             # self.get_logger().info('service not available, waiting again...')
-            print('service 3 not available, waiting again...')
+            print('run_trial service not available, waiting again...')
         while not self.move_in_client.wait_for_service(timeout_sec=1.0):
             # self.get_logger().info('service not available, waiting again...')
-            print('service 4 not available, waiting again...')
+            print('move_in service not available, waiting again...')
         while not self.move_out_client.wait_for_service(timeout_sec=1.0):
             # self.get_logger().info('service not available, waiting again...')
-            print('service 5 not available, waiting again...')
+            print('move_out service not available, waiting again...')
 
+        # This creates the bag recorder and provides all the topics that need to be saved to the bag file
         self.bag_recorder = BagRecorder(['/blueberry_dc/load_cell_data', 
                                          '/camera/depth/camera_info',
                                          '/camera/depth/image_rect_raw',
@@ -77,11 +97,12 @@ class ROSDataSubscriber(Node):
         self.subscription = self.create_subscription(Float64MultiArray, '/blueberry_dc/load_cell_data', self.data_callback, 10)
 
 
-
+    # This function gets the time that the load cell messages were received
     def data_callback(self, msg):
         self.last_load_cell_message_time = self.get_clock().now()
         self.load_cell_force = msg.data[2]
 
+    # This function checks to see if there is still data being published from the load cell
     def load_cell_publishing(self):
         current_time = self.get_clock().now()
         if self.last_load_cell_message_time is None:
@@ -91,12 +112,14 @@ class ROSDataSubscriber(Node):
             return False  # No publishing activity
         return True  # Active publishing
 
+    # This function sends the requests 
     def send_command(self, move_client):
         request = Trigger.Request()
         response = move_client.call_async(request)
 
-
+# This class sets up the graphical user interface (GUI)
 class MainWindow(QWidget):
+    # This function initializes the variables
     def __init__(self, ros_node):
         super().__init__()
 
@@ -104,6 +127,7 @@ class MainWindow(QWidget):
 
         self.initUI()
 
+    # This function initilizes the user interface
     def initUI(self):
         layout = QVBoxLayout(self)
 
@@ -221,21 +245,25 @@ class MainWindow(QWidget):
 
         self.setLayout(layout)
 
+    # This function sends commands based on the up button being clicked
     def upButtonClicked(self):
         self.ros_node.send_command(self.ros_node.move_up_client)
         self.actuator_height_box.setValue(self.actuator_height_box.value() + 5)
 
-
+    # This function sends commands based on the down button being clicked
     def downButtonClicked(self):
         self.ros_node.send_command(self.ros_node.move_down_client)
         self.actuator_height_box.setValue(self.actuator_height_box.value() - 5)
 
+    # This function sends commands based on the in button being clicked
     def inButtonClicked(self):
         self.ros_node.send_command(self.ros_node.move_in_client)
 
+    # This function sends commands based on the out button being clicked
     def outButtonClicked(self):
         self.ros_node.send_command(self.ros_node.move_out_client)
 
+    # This function sends commands based on the start trial button being clicked
     def startTrialClicked(self):
         bag_name = self.get_file_name()
         if self.check_for_existing_bag(bag_name):
@@ -253,6 +281,7 @@ class MainWindow(QWidget):
         self.ros_node.bag_recorder.stop_recording()
         self.log_window.appendPlainText(f"Trial finished. Bag file saved as: {bag_name}")
 
+    # This function updates the power bar with the most recent force value
     def updatePowerBar(self):
         force_value = int(self.ros_node.load_cell_force)
         self.powerBar.setValue(force_value)
@@ -260,7 +289,7 @@ class MainWindow(QWidget):
         # force update of app
         QApplication.processEvents()
 
-
+    # This function sets the file name based on the bush number, branch number, trial number, and height
     def get_file_name(self):
         bush_num = self.bush_num_box.value()
         branch_num = self.branch_num_box.value()
@@ -269,6 +298,7 @@ class MainWindow(QWidget):
         bag_name = self.ros_node.bag_file_dir + 'bush_' + str(bush_num) + '_branch_' + str(branch_num) + '_trial_' + str(trial_num) + '_height_' + str(actuactor_height) 
         return bag_name
     
+    # This function checks to see if a file of that name already exists
     def check_for_existing_bag(self, bag_name):
         bag_name_to_check_for = bag_name.split('_height')[0] + '*'
         matching_bag_names = glob.glob(bag_name_to_check_for)
@@ -278,9 +308,11 @@ class MainWindow(QWidget):
         else:
             return False
 
+# This function runs the ROS node
 def run_data_subscriber(ros_node):
     rclpy.spin(ros_node)
-    
+
+# This is the main function     
 def main():
     
     rclpy.init()
